f514b0599ed162b498bd6b3ce1354dca78385f92
zouzhibin <3517876561@qq.com>
2021-09-12 22:09:37 +0800

    提交数据

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,8 @@
 // 表示这是一个文本元素
 export const ELEMENT_TEXT = Symbol.for('ELEMENT_TEXT')
+// React应用需要一个根Fiber
+export const TAG_ROOT = Symbol.for('TAG_ROOT')
+// 原生的节点 span div p 函数组件 类组件
+export const TAG_HOST = Symbol.for('TAG_HOST')
+// 这是文本节点
+export const ELEMENT_TEXT = Symbol.for('TAG_TEXT')
